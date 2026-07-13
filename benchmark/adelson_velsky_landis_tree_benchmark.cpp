b9ead51db39a1f549fd217e95c5d3cec3d3738bc
NguyenPNhan <nguyenphunhan1110@gmail.com>
2026-07-13 19:18:43 +0700

    document running time of 2 data structures

--- benchmark/adelson_velsky_landis_tree_benchmark.cpp
+++ benchmark/adelson_velsky_landis_tree_benchmark.cpp
@@ -5,3 +5,58 @@ int main() {
         "Adelson-Velsky-Landis Tree"
     );
 }
+/*
+size: 1000
+random: Adelson-Velsky-Landis Tree - 0.415ms
+increasing: Adelson-Velsky-Landis Tree - 0.354ms
+small shuffle: Adelson-Velsky-Landis Tree - 0.363ms
+remove: Adelson-Velsky-Landis Tree - 0.383ms
+search all: Adelson-Velsky-Landis Tree - 0.135ms
+small search large insert: Adelson-Velsky-Landis Tree - 0.013ms
+large search small insert: Adelson-Velsky-Landis Tree - 0.087ms
+
+size: 10000
+random: Adelson-Velsky-Landis Tree - 6.269ms
+increasing: Adelson-Velsky-Landis Tree - 5.298ms
+small shuffle: Adelson-Velsky-Landis Tree - 6.986ms
+remove: Adelson-Velsky-Landis Tree - 5.008ms
+search all: Adelson-Velsky-Landis Tree - 1.840ms
+small search large insert: Adelson-Velsky-Landis Tree - 0.117ms
+large search small insert: Adelson-Velsky-Landis Tree - 1.370ms
+
+size: 20000
+random: Adelson-Velsky-Landis Tree - 11.726ms
+increasing: Adelson-Velsky-Landis Tree - 13.469ms
+small shuffle: Adelson-Velsky-Landis Tree - 10.813ms
+remove: Adelson-Velsky-Landis Tree - 14.064ms
+search all: Adelson-Velsky-Landis Tree - 3.755ms
+small search large insert: Adelson-Velsky-Landis Tree - 0.267ms
+large search small insert: Adelson-Velsky-Landis Tree - 2.920ms
+
+size: 50000
+random: Adelson-Velsky-Landis Tree - 37.101ms
+increasing: Adelson-Velsky-Landis Tree - 26.823ms
+small shuffle: Adelson-Velsky-Landis Tree - 37.630ms
+remove: Adelson-Velsky-Landis Tree - 34.161ms
+search all: Adelson-Velsky-Landis Tree - 13.565ms
+small search large insert: Adelson-Velsky-Landis Tree - 1.242ms
+large search small insert: Adelson-Velsky-Landis Tree - 11.540ms
+
+size: 100000
+random: Adelson-Velsky-Landis Tree - 118.988ms
+increasing: Adelson-Velsky-Landis Tree - 75.040ms
+small shuffle: Adelson-Velsky-Landis Tree - 103.462ms
+remove: Adelson-Velsky-Landis Tree - 123.518ms
+search all: Adelson-Velsky-Landis Tree - 33.435ms
+small search large insert: Adelson-Velsky-Landis Tree - 4.460ms
+large search small insert: Adelson-Velsky-Landis Tree - 37.146ms
+
+size: 200000
+random: Adelson-Velsky-Landis Tree - 209.581ms
+increasing: Adelson-Velsky-Landis Tree - 125.422ms
+small shuffle: Adelson-Velsky-Landis Tree - 140.832ms
+remove: Adelson-Velsky-Landis Tree - 193.093ms
+search all: Adelson-Velsky-Landis Tree - 60.978ms
+small search large insert: Adelson-Velsky-Landis Tree - 4.495ms
+large search small insert: Adelson-Velsky-Landis Tree - 40.908ms
+*/
--- benchmark/binary_heap_benchmark.cpp
+++ benchmark/binary_heap_benchmark.cpp
@@ -3,3 +3,58 @@
 int main() {
     return benchmark::runAllBenchmarks<BinaryHeap>("Binary Heap");
 }
+
+/*
+random: Binary Heap - 1.066ms
+increasing: Binary Heap - 0.925ms
+small shuffle: Binary Heap - 0.888ms
+remove: Binary Heap - 0.525ms
+search all: Binary Heap - 0.715ms
+small search large insert: Binary Heap - 0.106ms
+large search small insert: Binary Heap - 0.249ms
+
+size: 10000
+random: Binary Heap - 114.352ms
+increasing: Binary Heap - 110.027ms
+small shuffle: Binary Heap - 91.101ms
+remove: Binary Heap - 55.869ms
+search all: Binary Heap - 72.992ms
+small search large insert: Binary Heap - 6.865ms
+large search small insert: Binary Heap - 13.074ms
+
+size: 20000
+random: Binary Heap - 355.856ms
+increasing: Binary Heap - 305.276ms
+small shuffle: Binary Heap - 317.014ms
+remove: Binary Heap - 137.042ms
+search all: Binary Heap - 265.826ms
+small search large insert: Binary Heap - 26.075ms
+large search small insert: Binary Heap - 58.054ms
+
+size: 50000
+random: Binary Heap - 1786.387ms
+increasing: Binary Heap - 1766.701ms
+small shuffle: Binary Heap - 1659.293ms
+remove: Binary Heap - 822.321ms
+search all: Binary Heap - 1619.099ms
+small search large insert: Binary Heap - 154.020ms
+large search small insert: Binary Heap - 311.854ms
+
+size: 100000
+random: Binary Heap - 6625.843ms
+increasing: Binary Heap - 6528.300ms
+small shuffle: Binary Heap - 6636.951ms
+remove: Binary Heap - 3537.029ms
+search all: Binary Heap - 12889.077ms
+small search large insert: Binary Heap - 1476.098ms
+large search small insert: Binary Heap - 2882.314ms
+
+size: 200000
+random: Binary Heap - 49268.174ms
+increasing: Binary Heap - 26365.471ms
+small shuffle: Binary Heap - 46907.010ms
+remove: Binary Heap - 23781.746ms
+search all: Binary Heap - 49491.568ms
+small search large insert: Binary Heap - 3108.333ms
+large search small insert: Binary Heap - 5866.317ms
+*/
